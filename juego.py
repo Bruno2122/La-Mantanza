20d83975caf2970fb150a40ce9ae734377c84b31
Bruno2122 <bmontenegro@alumno.huergo.edu.ar>
2026-09-05 18:15:23 -0300

    parche 0.0.1

--- juego.py
+++ juego.py
@@ -10,3 +10,10 @@ def menu():
     fondo = pygame.transform.scale(fondo, (info_pantalla.current_w, info_pantalla.current_h))
     pygame.draw.rect(pantalla, (255, 0, 0), (x, y, 100, 100))
     pygame.display.flip()
+    ejecutar = True
+    while ejecutar:
+
+        pantalla.blit(fondo, (0, 0))
+        pygame.draw.rect(pantalla, (0, 255, 0), (200, 765, 150, 100))
+        pygame.draw.rect(pantalla, (255, 0, 0), (x, y, 100, 100))
+        pygame.display.flip()
