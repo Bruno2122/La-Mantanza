393509e5344d452e049b4ca81679707ecaeff152
Bruno2122 <bmontenegro@alumno.huergo.edu.ar>
2026-09-05 18:37:51 -0300

    0.0.3

--- juego.py
+++ juego.py
@@ -3,9 +3,14 @@ def menu():
     pygame.init()
     info_pantalla = pygame.display.Info()
     pantalla = pygame.display.set_mode((info_pantalla.current_w, info_pantalla.current_h), pygame.FULLSCREEN)
+    x = info_pantalla.current_w  
+    y = info_pantalla.current_h
+    mid_x = x/2
+    mid_y = y/2
     pygame.display.set_caption("El prime")
     fondo = pygame.image.load("fondo menu.png").convert()
     fondo = pygame.transform.scale(fondo, (info_pantalla.current_w, info_pantalla.current_h))
+    rec1 = pygame.draw.rect()
     pygame.display.flip()
     ejecutar = True
     while ejecutar:
