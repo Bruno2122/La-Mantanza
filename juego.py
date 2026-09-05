8cab911f42c81aacbc00fd2b7489ecb56a19df35
Bruno2122 <bmontenegro@alumno.huergo.edu.ar>
2026-09-05 18:13:03 -0300

    Update juego.py

--- juego.py
+++ juego.py
@@ -1,1 +1,12 @@
 import pygame
+def menu():
+    x = 835
+    y = 765
+    pygame.init()
+    info_pantalla = pygame.display.Info()
+    pantalla = pygame.display.set_mode((info_pantalla.current_w, info_pantalla.current_h), pygame.FULLSCREEN)
+    pygame.display.set_caption("El prime")
+    fondo = pygame.image.load("imagen.jpg").convert()
+    fondo = pygame.transform.scale(fondo, (info_pantalla.current_w, info_pantalla.current_h))
+    pygame.draw.rect(pantalla, (255, 0, 0), (x, y, 100, 100))
+    pygame.display.flip()
