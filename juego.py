8e23b8734d983a14c0408463b81a020904fb663f
Bruno2122 <bmontenegro@alumno.huergo.edu.ar>
2026-09-05 18:23:36 -0300

    parche 0.0.2

--- juego.py
+++ juego.py
@@ -1,19 +1,18 @@
 import pygame
 def menu():
-    x = 835
-    y = 765
     pygame.init()
     info_pantalla = pygame.display.Info()
     pantalla = pygame.display.set_mode((info_pantalla.current_w, info_pantalla.current_h), pygame.FULLSCREEN)
     pygame.display.set_caption("El prime")
-    fondo = pygame.image.load("imagen.jpg").convert()
+    fondo = pygame.image.load("fondo menu.png").convert()
     fondo = pygame.transform.scale(fondo, (info_pantalla.current_w, info_pantalla.current_h))
-    pygame.draw.rect(pantalla, (255, 0, 0), (x, y, 100, 100))
     pygame.display.flip()
     ejecutar = True
     while ejecutar:
-
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                ejecutar = False
         pantalla.blit(fondo, (0, 0))
-        pygame.draw.rect(pantalla, (0, 255, 0), (200, 765, 150, 100))
-        pygame.draw.rect(pantalla, (255, 0, 0), (x, y, 100, 100))
         pygame.display.flip()
+    pygame.quit()
+menu()
